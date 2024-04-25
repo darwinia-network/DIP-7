db2d9536c607d45247bd21f9d9c3573049304cda
echo <hujw77@gmail.com>
2024-04-25 17:03:54 +0800

    deposit & redeem

--- src/CollatorStaking.sol
+++ src/CollatorStaking.sol
@@ -1,8 +1,144 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "./StakingRewards.sol";
+contract CollatorStaking {
+    enum Rounding {
+        ROUND_DOWN,
+        ROUND_UP
+    }
 
-contract CollatorStaking is StakingRewards {
-    constructor(address _rewardsDistribution, address wring) StakingRewards(_rewardsDistribution, wring) {}
+    // assert nft
+    IERC721 nft;
+    // assert ft(ring) + assert nft
+    uint256 totalAssets;
+
+    // share ft
+    IERC20 stRING;
+    // share nft
+    IERC721 stNFT;
+    // share ft + share nft
+    uint256 totalShares;
+
+    event Deposit(address recipient, uint256 assets, uint256 shares);
+    event DepositNFT(address recipient, uint256 assets, uint256 shares);
+    event Withdraw(address sender, uint256 assets, uint256 shares);
+    event WithdrawNFT(address sender, uint256 assets, uint256 shares);
+
+    constructor() {}
+
+    receive() payable nonreentrant {
+        uint256 assets = msg.value;
+        require(assets > 0);
+        totalAssets += assets;
+    }
+
+    function depositFT() external payable nonreentrant {
+        uint256 assets = msg.value;
+        address recipient = msg.sender;
+        require(assets > 0);
+
+        uint256 shares = _issueSharesForAmount(assets, recipient, false);
+
+        totalAssets += assets;
+
+        require(shares > 0);
+
+        totalPowers += assets;
+
+        emit Deposit(recipient, assets, shares);
+    }
+
+    function redeem(uint256 shares) external nonreentrant {
+        uint256 assets = _convert_to_assets(shares, Rounding.ROUND_DOWN);
+        require(shares > 0 && assets > 0);
+        _burnShares(shares, msg.sender);
+        totalAssets -= assets;
+        msg.sender.call{value: assets}();
+
+        emit Withdraw(msg.sender, assets, shares);
+    }
+
+    function redeemNFT(uint256 nftId) external nonreentrant {
+        uint256 shares = stNFT.sharesOf(nftId);
+        uint256 assets = _convert_to_assets(shares, Rounding.ROUND_DOWN);
+        require(shares > 0 && assets > 0);
+        _burnNFTShares(shares, nftId);
+        totalAssets -= assets;
+        msg.sender.call{value: assets}();
+    }
+
+    function depositNFT(uint256 nftId) external nonreentrant {
+        uint256 assets = nft.assetOf(nftId);
+        address recipient = msg.sender;
+        nft.transferFrom(msg.sender, address(this), nftId);
+
+        uint256 shares = _issueSharesForAmount(assets, recipient, true);
+
+        totalPowers += assets;
+        emit DepositNFT(recipient, assets, shares);
+    }
+
+    function _issueSharesForAmount(uint256 amount, address recipient, bool isNFT)
+        internal
+        returns (uint256 newShares)
+    {
+        if (totalShares == 0) {
+            newShares = amount;
+        } else {
+            newShares = amount * totalShares / totalAssets;
+        }
+
+        if (newShares == 0) {
+            return 0;
+        }
+
+        if (isNFT) {
+            _issueNFTShares(newShares, recipient);
+        } else {
+            _issueShares(newShares, recipient);
+        }
+    }
+
+    function _issueShares(uint256 shares, address recipient) internal {
+        stRING.mint(recipient, shares);
+        totalShares += shares;
+    }
+
+    function _burnShares(uint256 shares, address from) internal {
+        stRING.burn(from, shares);
+        totalShares -= shares;
+    }
+
+    function _issueNFTShares(uint256 shares, address recipient) internal {
+        stNFT.mint(recipient, shares);
+        totalShares += shares;
+    }
+
+    function _burnNFTShares(uint256 shares, address from, uint256 nftId) internal {
+        stNFT.burn(from, nftId);
+        totalShares -= shares;
+    }
+
+    // assets = shares * (total_assets / total_supply) --- (== price_per_share * shares)
+    function _convert_to_assets(uint256 shares, Rounding rounding) internal view returns (uint256) {
+        require(shares > 0 && totalShares > 0);
+        uint256 numerator = shares * totalAssets;
+        uint256 assets = numerator / totalShares;
+
+        if (rounding == Rounding.ROUND_UP && numerator % totalShares != 0) {
+            assets += 1;
+        }
+        return assets;
+    }
+
+    // shares = amount * (total_supply / total_assets) --- (== amount / price_per_share)
+    function _convert_to_shares(uint256 assets, Rounding rounding) internal view returns (uint256) {
+        require(assets > 0 && totalAssets > 0);
+        uint256 numerator = assets * totalShares;
+        uint256 shares = numerator / totalAssets;
+        if (rounding == Rounding.ROUND_UP && numerator % totalAssets != 0) {
+            shares += 1;
+        }
+        return shares;
+    }
 }
--- src/CollatorStakingFactory.sol
+++ src/CollatorStakingFactory.sol
@@ -1,6 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+import "openzeppelin-solidity-4.9.6/contracts/utils/structs/EnumerableSet.sol";
 import "./CollatorStaking.sol";
 
-contract CollatorStakingFactory {}
+contract CollatorStakingFactory {
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    EnumerableSet.AddressSet private collators;
+    address public depositNFT;
+    address public distribution;
+    address public wring;
+
+    mapping(address => address) public collatorOf;
+
+    function createCollator() public {
+        address creator = msg.sender;
+        require(collatorOf[user] == address(0));
+        STRING stRing = new STRING();
+        CollatorStaking collator = new CollatorStaking(distribution, stRing, depositNFT, wring, creator);
+        require(collators.add(collator));
+    }
+}
