8719ecbad3f6bec3f24aa1db8a77131d2b769ec3
echo <hujw77@gmail.com>
2024-11-29 17:24:13 +0800

    migrate script

--- script/Migrate.s.sol
+++ script/Migrate.s.sol
@@ -7,14 +7,15 @@ import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 import {Core} from "openzeppelin-foundry-upgrades/internal/Core.sol";
 
 import {CollatorStakingHub} from "../src/collator/CollatorStakingHub.sol";
+import {Deposit} from "../src/deposit/Deposit.sol";
 
 contract MigrateScript is Script {
-    address proxy = 0xa4fFAC7A5Da311D724eD47393848f694Baee7930;
+    address proxy = 0x46275d29113f065c2aac262f34C7a3d8a8B7377D;
 
     function run() public {
         vm.startBroadcast();
 
-        address logic = address(new CollatorStakingHub());
+        address logic = address(new Deposit());
         Core.upgradeProxyTo(proxy, logic, "");
         require(logic == Upgrades.getImplementationAddress(proxy));
         safeconsole.log("logic: ", logic);
